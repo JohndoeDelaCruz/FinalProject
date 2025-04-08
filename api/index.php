6ea21907a8ebf333038950554e3c5db015b7b60e
JohndoeDelaCruz <krisjohndelacruz86@gmail.com>
2025-04-08 12:31:45 +0800

    revert changes

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Set the base path for the Laravel application
-$basePath = realpath(__DIR__ . '/../cms');
-
-// Change current directory to the Laravel root
-chdir($basePath);
-
-// Load the Laravel application
-require $basePath . '/public/index.php'; 
